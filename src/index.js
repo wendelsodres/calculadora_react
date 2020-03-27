3fde6193108a99b83999c394261d037d906e09b1
Wendel Sodre <wendel.sodre@itelios.com>
2020-03-27 08:57:37 -0300

    Finish calculator.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Caculator from './main/Calculator'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <Caculator />
+  </div>
+  ,
   document.getElementById('root')
 );
 
